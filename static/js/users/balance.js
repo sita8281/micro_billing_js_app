26c3b76ffe71aedee8c84887edd8c9c057474a63
adsitnikov <adsitnikov@fil-it.ru>
2025-05-20 14:43:43 +0300

    додела окно финансовых операций

--- static/js/users/balance.js
+++ static/js/users/balance.js
@@ -1,35 +1,50 @@
 import { httpClient } from "../api.js";
-import { notifyInfo, notifyWarning, getNodeTreeview, notifySuccess } from "../utils.js";
-import { windowWait } from "../windows/balance_operations.js";
+import { notifyInfo, notifyWarning, getNodeTreeview, notifySuccess, getSelectedUserId, notifyError } from "../utils.js";
+import { windowWait, windowBalanceUser, getSum, updateLabel, clearInputs} from "../windows/balance_operations.js";
 
 
 async function balanceUserWithServer() { 
-    const window = await windowCreateUser();
-    
+    const window = await windowBalanceUser();
+
+    if (!window) {
+        return;
+    }
 
     while (true) {
         const state = await windowWait();
-        if (!state) {
-            return;
-        }
-        const data = windowCreateUserGetData();
-        
-        const response = await httpClient.createUser({
-            folderId: data.folder,
-            poolId: Number(data.pool),
-            role: data.right,
-            login: data.login,
-            password: data.password,
-            name: data.name,
-        })
-        console.log(response);
-        
 
-        if (response.status == 201) {
-            notifySuccess(response.description);
+        try {
+            if (state == 'deposit') {
+                const response = await httpClient.userBalanceDeposit(getSelectedUserId(), getSum(), 'test')
+                if (response.status == 200) {
+                    notifySuccess(response.description);
+                    clearInputs();
+                    if (!await updateLabel()) {
+                        notifyError('Возникла ошибка при обновлении баланса');
+                    }
+                } else {
+                    notifyWarning(response.description)
+                }
+            } else if ( state == 'take') {
+                const response = await httpClient.userBalanceTake(getSelectedUserId(), getSum(), 'test')
+                if (response.status == 200) {
+                    notifySuccess(response.description);
+                    clearInputs();
+                    if (!await updateLabel()) {
+                        notifyError('Возникла ошибка при обновлении баланса');
+                    }
+                } else {
+                    notifyWarning(response.description)
+                }
+
+            } else {
+                return;
+            }
+
+        } catch {
             window.hide();
-        } else {
-            notifyWarning(response.description)
+            return;
+            
         }
         
     }
